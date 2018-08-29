8d761f7a7bee82e24620a91c0476b7885886d132
jesgra <jesus.egranada@gmail.com>
2018-08-29 13:56:50 -0500

    nueva categoria (test)

--- jenkins_tutorial/src/main/java/hello/CategoriaController.java
+++ jenkins_tutorial/src/main/java/hello/CategoriaController.java
@@ -16,11 +16,13 @@ public class CategoriaController {
 		Categoria c1 = new Categoria(1,"hoteles", "hoteles.html", "imagenes/hoteles.jpg");
 		Categoria c2 = new Categoria(2,"restaurantes", "hoteles.html", "imagenes/grafica2.jpg");
 		Categoria c3 = new Categoria(3,"tiendas", "hoteles.html", "imagenes/grafica3.jpg");
-		Categoria c4 = new Categoria(4,"comidas", "hoteles.html", "imagenes/grafica4.jpg");	
+		Categoria c4 = new Categoria(4,"comidas", "hoteles.html", "imagenes/grafica4.jpg");
+		Categoria c5 = new Categoria(4,"hospital", "hospital.html", "imagenes/grafica5.jpg");
         categorias.add(c1);
         categorias.add(c2);
         categorias.add(c3);
         categorias.add(c4);
+        categorias.add(c5);
         
 		return categorias; 
 		}
